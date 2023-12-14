7147451487d15f9bfc80a3047149561473ced42b
ryanman123 <54139829+ryanman123@users.noreply.github.com>
2023-12-14 15:51:11 +0000

    copilot third commit

---
+++ comments.js
@@ -0,0 +1,32 @@
+// Create web server
+// 1. Create a web server
+// 2. Create a route for the home page
+// 3. Create a route for the /comments page
+// 4. Create a route for the /comments/new page
+// 5. Create a route for the /comments/:id page
+// 6. Create a route for the /comments/:id/edit page
+// 7. Create a route for the /comments/:id/delete page
+// 8. Create a route for the /comments/:id/like page
+// 9. Create a route for the /comments/:id/dislike page
+// 10. Create a route for the /comments/:id/report page
+// 11. Create a route for the /comments/:id/unreport page
+// 12. Create a route for the /comments/:id/flag page
+// 13. Create a route for the /comments/:id/unflag page
+// 14. Create a route for the /comments/:id/pin page
+// 15. Create a route for the /comments/:id/unpin page
+// 16. Create a route for the /comments/:id/restore page
+// 17. Create a route for the /comments/:id/unrestore page
+// 18. Create a route for the /comments/:id/lock page
+// 19. Create a route for the /comments/:id/unlock page
+// 20. Create a route for the /comments/:id/subscribe page
+// 21. Create a route for the /comments/:id/unsubscribe page
+// 22. Create a route for the /comments/:id/reply page
+// 23. Create a route for the /comments/:id/unreply page
+// 24. Create a route for the /comments/:id/flagged page
+// 25. Create a route for the /comments/:id/reported page
+// 26. Create a route for the /comments/:id/pinned page
+// 27. Create a route for the /comments/:id/locked page
+// 28. Create a route for the /comments/:id/subscribed page
+// 29. Create a route for the /comments/:id/replied page
+// 30. Create a route for the /comments/:id/likes page
+// 31. Create a route for the /comments/:id
